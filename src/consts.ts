83ae8a998d9aaf005fb0fb118ee7279c9b71ead4
CZrich <czrichardz@gmail.com>
2025-12-18 21:29:45 -0500

    ultimos cambios

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Nuestro Blog';
+export const SITE_TITLE = 'Código Blanco';
 export const SITE_DESCRIPTION = 'Bienvenido  a este pequeño blog!';
